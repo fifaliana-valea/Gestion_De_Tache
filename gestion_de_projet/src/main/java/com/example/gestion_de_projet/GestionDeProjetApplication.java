40bbaccc27b9b8de6566ffa532676d5db462597e
Fifaliana_valea <valeafifaliana@gmail.com>
2024-12-02 16:58:11 +0300

    back

---
+++ gestion_de_projet/src/main/java/com/example/gestion_de_projet/GestionDeProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_de_projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeProjetApplication.class, args);
+	}
+
+}
